fa3616201413e407ce2d7fa4fb79a32fbbbb844b
YHJ <yhjin07@naver.com>
2024-03-26 17:23:16 +0900

    up

---
+++ DX_YHJ/Contents/SamCore.cpp
@@ -0,0 +1,16 @@
+#include "PreCompile.h"
+#include "SamCore.h"
+
+USamCore::USamCore() 
+{
+}
+
+USamCore::~USamCore() 
+{
+}
+
+void USamCore::Initialize()
+{
+	// GEngine->CreateLevel();
+	int a = 0;
+}
