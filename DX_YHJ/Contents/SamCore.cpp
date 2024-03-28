6daedb4917504cda0d5960cd215426ea9850b06b
YHJ <yhjin07@naver.com>
2024-03-28 14:10:19 +0900

    Up

--- DX_YHJ/Contents/SamCore.cpp
+++ DX_YHJ/Contents/SamCore.cpp
@@ -1,5 +1,6 @@
 #include "PreCompile.h"
 #include "SamCore.h"
+#include "PlayGameMode.h"
 
 USamCore::USamCore() 
 {
@@ -11,4 +12,19 @@ USamCore::~USamCore()
 
 void USamCore::Initialize()
 {
+	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
+	GEngine->ChangeLevel("PlayLevel");
+
+	//{
+	//	UEngineDirectory Dir;
+	//	Dir.MoveToSearchChild("ContentsResources");
+	//	Dir.Move("Sound");
+	//	std::list<UEngineFile> Files = Dir.AllFile({ ".wav" });
+	//	for (UEngineFile& File : Files)
+	//	{
+	//		UEngineSound::Load(File.GetFullPath());
+	//	}
+	//	UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+	//}
+
 }
