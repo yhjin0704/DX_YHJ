bd5ed3f6614e80ab9ffc3dcb8ede9ee14b92cacb
YHJ <yhjin07@naver.com>
2024-04-08 18:46:38 +0900

    Up

--- DX_YHJ/Contents/HoloCureCore.cpp
+++ DX_YHJ/Contents/HoloCureCore.cpp
@@ -20,8 +20,6 @@ void UHoloCureCore::Initialize()
 		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
 		for (UEngineFile& File : Files)
 		{
-			// CuttingTest.png texture로도 한장이 로드가 됐고
-			// 스프라이트로도 1장짜리로 로드가 된 상황이야.
 			UEngineSprite::Load(File.GetFullPath());
 		}
 
@@ -33,14 +31,6 @@ void UHoloCureCore::Initialize()
 			UEngineSprite::LoadFolder(Directorys[i].GetFullPath());
 		}
 
-		// 특정 스프라이트나 
-		// 특정 텍스처를 찾아서
-		// 만약 스프라이트가 존재하지 않는다면
-		// 이걸 사용하는 순간 만들어내고 자른다.
-		// 이미 이 이름을 가진 스프라이트가 존재한다.
-		// 그러면 기존의 스프라이트 데이터는 날려버리고
-		// 자른 스프라이트 데이터 변경한다.
-		UEngineSprite::CreateCutting("CuttingTest.png", 4, 3);
 	}
 
 
@@ -51,14 +41,8 @@ void UHoloCureCore::Initialize()
 		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".wav" });
 		for (UEngineFile& File : Files)
 		{
-			//File.Open(EIOOpenMode::Read, EIODataType::Binary);
-
-			//char Arr[100];
-			//File.Read(Arr, 100);
-
 			UEngineSound::Load(File.GetFullPath());
 		}
-		// UEngineSound::SoundPlay("anipang_ingame_wav.wav");
 	}
 
 }
